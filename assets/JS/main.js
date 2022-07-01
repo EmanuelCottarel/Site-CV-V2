1f8f45cf1ade9c4b226eeb1fcfbe27102f4c5ea8
EmanuelCottarel <103122152+EmanuelCottarel@users.noreply.github.com>
2022-07-01 16:06:18 +0200

    "Initial commit"
    - création page d'accueil
    -création navbar
    - création de la partie Portfolio

---
+++ assets/JS/main.js
@@ -0,0 +1,34 @@
+
+
+// Animation text header "machine à écrire"
+
+window.addEventListener("DOMContentLoaded", (event) => {
+    animate_text();
+  });
+  // -------------------
+  function animate_text() 
+  {
+    let delay = 100,
+        delay_start = 100,
+        contents,
+        letters;
+  
+    document.querySelectorAll(".animate-text").forEach(function (elem) {
+      contents = elem.textContent.trim();
+      elem.textContent = "";
+      letters = contents.split("");
+      elem.style.visibility = 'visible';
+  
+      letters.forEach(function (letter, index_1) {
+        setTimeout(function () {
+          // ---------
+          // effet machine à écrire (SIMPLE)
+          elem.textContent += letter;
+          
+  
+          // ---------
+        }, delay_start + delay * index_1);
+      });    
+      delay_start += delay * letters.length;
+    });
+  }
